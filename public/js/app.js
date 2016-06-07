1e39d458cd7fd8172bd2d0a2b829c0df0f11c2b1
Vicente <vincent_97@live.com.mx>
2016-06-07 16:48:14 -0500

    Se actualizo react a la version 15.1.0, se solucionaron problemas con la nueva version de superagent y la ruta delete(users)

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 var React = require('react'),
+    ReactDOM = require('react-dom'),
     Cms = require('./components/Cms.react');
 
 // export for http://fb.me/react-devtools, only in development mode
@@ -6,4 +7,4 @@ if (process.env.NODE_ENV === 'development') {
   window.React = React;
 }
 
-React.render(<Cms />, document.body);
+ReactDOM.render(<Cms />, document.getElementById('main'));
